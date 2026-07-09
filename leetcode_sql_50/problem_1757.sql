f8a9c3cc38abd66cb390896b2655ff36c567b548
Anandbanne <anandbanne43@gmail.com>
2026-07-09 09:36:31 +0530

    1757

---
+++ leetcode_sql_50/problem_1757.sql
@@ -0,0 +1,8 @@
+/*
+Problem : 1757. Recyclable and Low Fat Products
+Platform: LeetCode
+Difficulty: Easy
+Date: 21-06-2026
+*/
+select product_id from Products 
+where low_fats='Y' and recyclable='Y'; 
