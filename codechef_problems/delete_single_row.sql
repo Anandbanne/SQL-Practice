9097623a8a17b2f5551f543a8af6d53edd277e1a
Anandbanne <anandbanne43@gmail.com>
2026-07-16 10:51:03 +0530

    remove

---
+++ codechef_problems/delete_single_row.sql
@@ -0,0 +1,12 @@
+/*
+Practice - Delete Single Record
+
+Write a query which does the following
+
+Delete the entire row of the table 'customer' with customer id 1.
+Output the entire table once the above action is performed.
+Original table - 'customer' has the following rows*/
+
+delete from customer where Id=1;
+
+select *  from customer;
