367aab2dfd781924e01fc15465fdf3e9178b2dbb
Anandbanne <anandbanne43@gmail.com>
2026-07-31 10:45:32 +0530

    Add remaining Module-02-Where_Clause SQL files

---
+++ SQL_Mastery_Journey/Module-02-Where_Clause/or_operator_18.sql
@@ -0,0 +1,4 @@
+/*18. Display customers who are from Hyderabad OR Bangalore. */
+
+select * from customers where 
+city ='Hyderabad'or city ='Bangalore';
---
+++ SQL_Mastery_Journey/Module-02-Where_Clause/or_operator_20.sql
@@ -0,0 +1,4 @@
+/*20. Display all customers who are from Mumbai OR Chennai.*/
+
+select * from customers 
+where city='Chennai' or city='mumbai';
