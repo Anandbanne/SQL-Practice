f136a9beccbbe8546c7cd9449e6d8c07fbe97338
Anand Banne <anandbanne43@gmail.com>
2026-08-12 00:07:04 +0530

    Create SQL solution for Rising Temperature problem
    Add SQL query for problem 197: Rising Temperature.

---
+++ leetcode_sql_50/problem_197.sql
@@ -0,0 +1,9 @@
+/*
+ problem: 197. Rising Temperature
+ type: easy
+ date: 11/07/2026
+ 
+*/
+select w1.id from 
+weather w1 join weather w2 on w1.recorddate=date_add(w2.   recorddate, interval 1 day) 
+where w1.temperature >w2.temperature;
