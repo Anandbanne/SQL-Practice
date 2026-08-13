0e28ba794227ac598efae60d5bb91a08405aa031
Anand Banne <anandbanne43@gmail.com>
2026-08-14 00:24:33 +0530

    Add SQL query for LeetCode problem 619

---
+++ leetcode_sql_50/problem_619.sql
@@ -0,0 +1,9 @@
+/*
+Problem : 619. Biggest Single Number
+Platform: LeetCode
+Difficulty: Easy
+Date: 09-07-2026
+*/
+
+select max(num) as num from mynumbers where num in (
+select num from mynumbers group by num having count(num)=1 );
