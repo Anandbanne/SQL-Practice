6e07c377856e846edf87bf0f8837e4f02dd18dc9
Anand Banne <anandbanne43@gmail.com>
2026-08-20 22:20:34 +0530

    Add metadata comments for problem 601
    Added problem description and metadata for problem 601.

---
+++ leetcode_sql_50/problem_601.sql
@@ -0,0 +1,37 @@
+/*
+ problem   : 601. Human Traffic of Stadium
+ difficulty: hard
+ date      : 19/08/2026
+ platforom  : leetcode  
+*/
+
+SELECT DISTINCT s1.*
+FROM Stadium s1
+JOIN Stadium s2
+    ON s2.id = s1.id + 1
+JOIN Stadium s3
+    ON s3.id = s1.id + 2
+WHERE s1.people >= 100
+  AND s2.people >= 100
+  AND s3.people >= 100
+UNION
+SELECT DISTINCT s1.*
+FROM Stadium s1
+JOIN Stadium s2
+    ON s2.id = s1.id - 1
+JOIN Stadium s3
+    ON s3.id = s1.id - 2
+WHERE s1.people >= 100
+  AND s2.people >= 100
+  AND s3.people >= 100
+UNION
+SELECT DISTINCT s1.*
+FROM Stadium s1
+JOIN Stadium s2
+    ON s2.id = s1.id + 1
+JOIN Stadium s3
+    ON s3.id = s1.id - 1
+WHERE s1.people >= 100
+  AND s2.people >= 100
+  AND s3.people >= 100
+ORDER BY visit_date;
