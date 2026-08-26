9273a546f7109db805e670218d9f1135fea3ea6d
Anand Banne <anandbanne43@gmail.com>
2026-08-26 12:34:23 +0530

    Scaler Subqueries

--- SQL_Mastery_Journey/Module_6_Subqueries/basic_lvl_queries.sql
+++ SQL_Mastery_Journey/Module_6_Subqueries/basic_lvl_queries.sql
@@ -2,3 +2,14 @@
 
 select product_name,price from products where 
   price  > (select avg(price) from products);
+
+/*2. The management team wants to identify the most expensive product in the catalog. 
+Return the product name and its price.*/
+select product_name,price from products where 
+  price = (select max(price) from products);
+
+
+/*3. The finance team wants to identify orders whose total amount is greater than the average order amount. 
+They want to review unusually high-value orders.*/
+select order_id,total_amount from orders where 
+  total_amount >(select avg(total_amount) from orders);
